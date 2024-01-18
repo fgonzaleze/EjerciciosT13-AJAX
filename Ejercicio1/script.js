9a560ca3d08baafcc4d6be2159bcfff88ff05805
jgonzalez <jgonzaleze@iesnervion.es>
2024-01-18 12:00:35 +0100

    Ejercicio 1 ejemplo

---
+++ Ejercicio1/script.js
@@ -0,0 +1,27 @@
+window.onload = inicializaEventos
+
+function inicializaEventos(){
+    let button = document.getElementById("btnSaludar");
+    button.addEventListener("click", saludar, false);
+}
+
+function saludar (){
+    let millamada = new XMLHttpRequest();
+    let divmensaje = document.getElementById("divmensaje");
+
+    millamada.open("GET", "hola.html")
+    millamada.onreadystatechange = function(){
+        if (millamada.readyState < 4){
+            divmensaje.innerHTML = "Cargando..."
+        } else  {
+            if (millamada.readyState == 4 && millamada.status == 200){
+                let response = millamada.responseText;
+                divmensaje.innerHTML = response;
+            }
+        };
+       
+    }
+    millamada.send()
+}
+
+let request = new XMLHttpRequest();
